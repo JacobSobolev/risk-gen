cfc3be8922fd394bbe421d65a413adfd41c5b8c3
Jacob Sobolev <yakovsobolev1@gmail.com>
2024-09-14 20:02:49 +0300

    create semi-real data using gpt

---
+++ app/api/data.py
@@ -0,0 +1,23 @@
+# device type: possible manufacturer
+device_manufacturers = {
+    "Firewall": ["Cisco Systems", "Fortinet", "Palo Alto Networks", "SonicWall", "Check Point Software"],
+    "Router": ["Cisco Systems", "Juniper Networks", "Huawei Technologies", "Ubiquiti Networks", "MikroTik"],
+    "Switch": ["Cisco Systems", "Arista Networks", "Hewlett Packard Enterprise (HPE)", "Extreme Networks", "Netgear"],
+    "Load Balancer": ["F5 Networks", "Citrix", "A10 Networks", "Radware", "Barracuda Networks"],
+    "Server": ["Dell Technologies", "Hewlett Packard Enterprise (HPE)", "Lenovo", "Huawei Technologies", "Cisco Systems"],
+    "Network Gateway": ["Cisco Systems", "Juniper Networks", "Fortinet", "Palo Alto Networks", "Zyxel Communications"],
+    "Access Point": ["Cisco Systems", "Ubiquiti Networks", "Aruba Networks", "Netgear", "Zyxel Communications"],
+    "Virtual Machine": ["VMware", "Microsoft", "Citrix", "Oracle", "Red Hat"],
+    "Proxy Server": ["F5 Networks", "Blue Coat Systems", "Squid", "Cisco Systems", "Barracuda Networks"],
+    "Intrusion Detection System (IDS)": ["Cisco Systems", "Palo Alto Networks", "Fortinet", "Snort", "McAfee"],
+    "Unified Threat Management (UTM)": ["Fortinet", "Sophos", "Cisco Systems", "WatchGuard", "Check Point Software"],
+    "VPN Gateway": ["Cisco Systems", "Palo Alto Networks", "Fortinet", "SonicWall", "Barracuda Networks"],
+    "Storage Area Network (SAN)": ["Dell Technologies", "Hewlett Packard Enterprise (HPE)", "NetApp", "IBM", "Hitachi"],
+    "Web Application Firewall (WAF)": ["F5 Networks", "Imperva", "Barracuda Networks", "Fortinet", "Akamai"],
+    "Endpoint Detection and Response (EDR)": ["CrowdStrike", "SentinelOne", "McAfee", "Symantec", "Carbon Black"],
+    "Email Security Gateway": ["Barracuda Networks", "Proofpoint", "Cisco Systems", "Mimecast", "Fortinet"],
+    "Data Loss Prevention (DLP)": ["Symantec", "McAfee", "Digital Guardian", "Forcepoint", "Cisco Systems"],
+    "Identity and Access Management (IAM)": ["Okta", "Microsoft", "Ping Identity", "IBM", "SailPoint"],
+    "Cloud Access Security Broker (CASB)": ["Netskope", "McAfee", "Microsoft", "Symantec", "Bitglass"],
+    "Next-Generation Firewall (NGFW)": ["Palo Alto Networks", "Fortinet", "Cisco Systems", "Juniper Networks", "SonicWall"]
+}
