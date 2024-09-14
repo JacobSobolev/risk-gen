8fa14c3a50ffa71c4e8e723755712867433d8fc2
Jacob Sobolev <yakovsobolev1@gmail.com>
2024-09-14 21:25:02 +0300

    remove redundant function, return 10 records

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,22 +4,21 @@ from app.api.api_utils import (random_device_and_manufacturer, random_ipv4, rand
 
 api_bp = Blueprint("api", __name__)
 
-import random
-
-def generate_random_ipv4():
-    return ".".join(str(random.randint(0, 255)) for _ in range(4))
-
 @api_bp.route("/report")
 def gen_report():
-    device_type, manufacturer = random_device_and_manufacturer()
-    vulnerability, vulnerability_desc = random_vulnerability_and_description()
-    record = {
-        "device_type": device_type,
-        "manufacturer": manufacturer,
-        "ip": random_ipv4(),
-        "date": random_time_in_past_month(),
-        "vulnerability": vulnerability,
-        "vulnerability_desc": vulnerability_desc,
-        "risk_level": random_risk_level()
-    }
-    return record
+    num_of_records = 10
+    records = []
+    for _ in range(num_of_records):
+        device_type, manufacturer = random_device_and_manufacturer()
+        vulnerability, vulnerability_desc = random_vulnerability_and_description()
+        record = {
+            "device_type": device_type,
+            "manufacturer": manufacturer,
+            "ip": random_ipv4(),
+            "date": random_time_in_past_month(),
+            "vulnerability": vulnerability,
+            "vulnerability_desc": vulnerability_desc,
+            "risk_level": random_risk_level()
+        }
+        records.append(record)
+    return records
