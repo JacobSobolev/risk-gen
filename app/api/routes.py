4415b62c6836f22e96b255c023e050c126e13f54
Jacob Sobolev <yakovsobolev1@gmail.com>
2024-09-14 21:35:11 +0300

    update the range for records param

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,7 @@ from app.api.api_utils import (random_device_and_manufacturer, random_ipv4, rand
                                random_vulnerability_and_description, random_risk_level)
 
 DEFUALT_NUM_RECORDS = 10
+MAX_NUM_RECORDS = 100
 
 api_bp = Blueprint("api", __name__)
 
@@ -11,6 +12,8 @@ def gen_report():
     try:
         num_records = int(request.args.get('records', default=DEFUALT_NUM_RECORDS))
     except ValueError:
+        num_records = DEFUALT_NUM_RECORDS
+    if num_records < 0 or num_records > MAX_NUM_RECORDS:
         num_records = DEFUALT_NUM_RECORDS
 
     records = []
